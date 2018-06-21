c84fd1a90b19b6f9a72ee0b021546e6c0a1a029b
Tomasz Fijalkowski <tomelecf@gmail.com>
2018-06-21 03:04:12 +0200

    Added animations

--- static/script.js
+++ static/script.js
@@ -8,7 +8,7 @@ const ARRAY = [
     'Q', 'Q', 'R', 'R', 'S', 'S', 'T', 'T',
     'U', 'U', 'V', 'V', 'W', 'W', 'X', 'X',
     'Y', 'Y', 'Z', 'Z', 'AA', 'AA', 'BB', 'BB',
-    'CC', 'CC', 'DD', 'DD', 'EE', 'EE', 'FF','FF',
+    'CC', 'CC', 'DD', 'DD', 'EE', 'EE', 'FF', 'FF',
 ];
 const ICONS = {
     A: "fas fa-crow",
@@ -50,10 +50,10 @@ const ICONS = {
     FF: "fas fa-dice-six"
 }
 
-
 let flippedCounter = 0;
 let flippedIds = [];
 let flipped = [];
+let endGame = false;
 
 function shuffle(array) {
     let currentIndex = array.length,
@@ -70,20 +70,20 @@ function shuffle(array) {
 }
 
 
-function newArray(){
-    return shuffle(ARRAY.slice(0,X*Y));
+function newArray() {
+    return shuffle(ARRAY.slice(0, X * Y));
 }
 
 
-function newGameBoard(){
+function newGameBoard() {
     let array = newArray();
     let gameBoard = document.getElementById("gameboard");
     let i = 0;
-    for (var row = 0; row < Y; row++){
+    for (var row = 0; row < Y; row++) {
         let rowElem = document.createElement("div");
         rowElem.setAttribute("class", "row");
         gameBoard.appendChild(rowElem);
-        for (var column = 0; column < X; column++){
+        for (var column = 0; column < X; column++) {
             let scene = document.createElement("div");
             let card = document.createElement("div");
             let cardFront = document.createElement("div");
@@ -92,13 +92,13 @@ function newGameBoard(){
             let icon = document.createElement("i");
             scene.setAttribute("class", "scene");
             card.setAttribute("data-flipped", "false");
-            card.setAttribute("class", "card hover");
-            card.setAttribute("id", 'card_'+i);
-            card.setAttribute("onClick", 'flip(this,\''+array[i]+'\','+array.length+')');
+            card.setAttribute("class", "card js-fade-card fade-in-card is-paused");
+            card.setAttribute("id", 'card_' + i);
+            card.setAttribute("onClick", 'flip(this,\'' + array[i] + '\',' + array.length + ')');
             cardFront.setAttribute("class", "card__face card__face--front");
             cardBack.setAttribute("class", "card__face card__face--back");
             icon.setAttribute("class", "icon " + ICONS[array[i]]);
-            
+
             cardFront.appendChild(empty);
             cardBack.appendChild(icon);
             rowElem.appendChild(scene);
@@ -108,13 +108,32 @@ function newGameBoard(){
             i++;
         }
     }
+    fadeInCards()
 }
 
+function fadeInCards() {
+    let elements = document.querySelectorAll('.js-fade-card');
+    for (let i = 0; i < elements.length; ++i) {
+        setInterval(function () {
+            if (elements[i].classList.contains('is-paused')) {
+                elements[i].classList.remove('is-paused');
+            }
+        }, i * 30);
+    }
+    for (let i = 0; i < elements.length; ++i) {
+        setInterval(function () {
+            if (elements[i].classList.contains('fade-in-card')) {
+                elements[i].setAttribute("class", "card hover js-fade-card is-paused");
+            }
+        }, (i * 30) + 600);
+    }
+}
 
-function flip(card, value, arrayLength){
-    if (card.dataset.flipped == "false" && flipped.length < 2){
+function flip(card, value, arrayLength) {
+    if (card.dataset.flipped == "false" && flipped.length < 2) {
+        card.dataset.flipped = "true";
         card.setAttribute("class", "card hover is-flipped");
-        if (flipped.length == 0){
+        if (flipped.length == 0) {
             flipped.push(value);
             flippedIds.push(card.id);
             document.getElementById(flippedIds[0]).classList.remove('hover');
@@ -122,19 +141,21 @@ function flip(card, value, arrayLength){
             flipped.push(value);
             flippedIds.push(card.id);
             document.getElementById(flippedIds[1]).classList.remove('hover');
-            if (flipped[0] == flipped[1]){
+            if (flipped[0] == flipped[1]) {
                 flippedCounter += 2;
                 flippedIds = [];
                 flipped = [];
                 if (flippedCounter == arrayLength) {
-                    alert("You won!");
+                    won();
                 }
             } else {
-                function flipBack(){
+                function flipBack() {
                     let firstCard = document.getElementById(flippedIds[0]);
                     let secondCard = document.getElementById(flippedIds[1]);
                     firstCard.setAttribute("class", "card hover");
                     secondCard.setAttribute("class", "card hover");
+                    firstCard.dataset.flipped = "false";
+                    secondCard.dataset.flipped = "false";
                     flippedIds = [];
                     flipped = [];
                 }
@@ -144,10 +165,75 @@ function flip(card, value, arrayLength){
     }
 }
 
-function setHudWidth(){
+function setHudWidth() {
     let width = document.getElementById("gameboard").offsetWidth;
     let hud = document.getElementById("hud");
-    hud.style.width = width+"px";
+    hud.style.width = width + "px";
     hud.style.transition = "1s";
     hud.style.color = "#000";
+}
+
+function countTime() {
+    let s = 0;
+    let m = 0;
+    let h = 0;
+    function seconds() {
+        if (!endGame) {
+            if (s < 59) {
+                s++;
+            } else {
+                s = 0;
+                if (m < 59) {
+                    m++;
+                } else {
+                    m = 0;
+                    h++;
+                }
+            }
+            displayTime(h, m, s)
+        }
+    }
+    setInterval(seconds, 1000);
+}
+
+function displayTime(h, m, s) {
+    let seconds = (s < 10) ? "0" + s : s;
+    let minutes = (m < 10) && (h > 0) ? "0" + m : m;
+    let hours = h;
+    let time;
+    if (h > 0) {
+        time = hours + ":" + minutes + ":" + seconds;
+    } else {
+        time = minutes + ":" + seconds;
+    }
+    document.getElementById("time").innerHTML = time;
+}
+
+function won() {
+    endGame = true;
+    let time = document.getElementById("time").innerHTML;
+    let modal = document.getElementById('myModal');
+    let elements = document.querySelectorAll('.js-fade');
+    modal.style.display = "block";
+    for (let i = 0; i < elements.length; ++i) {
+        if (elements[i].classList.contains('is-paused')) {
+            elements[i].classList.remove('is-paused');
+        }
+    }
+    document.getElementById("modalTimeNumber").innerHTML = time;
+    // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function (event) {
+    //     if (event.target == modal) {
+    //         modal.style.display = "none";
+    //     }
+    // }
+}
+
+function menuFadeIn() {
+    let elements = document.querySelectorAll('.js-fade');
+    for (let i = 0; i < elements.length; ++i) {
+        if (elements[i].classList.contains('is-paused')) {
+            elements[i].classList.remove('is-paused');
+        }
+    }
 }
